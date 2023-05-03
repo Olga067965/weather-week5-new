4ea2fe4564f002854108515dc649d46c10e7c772
Olga067965 <olgasavelieva5@gmail.com>
2023-05-03 13:09:20 +0300

    update3

--- src/index.js
+++ src/index.js
@@ -33,22 +33,11 @@ const temperature = document.getElementById('temperature');
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
   }
-  
-  function formatDay(timestamp) {
-    let date = new Date(timestamp * 1000);
-    let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  
-    return days[day];
-  }
 
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   
 
@@ -60,7 +49,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
+  let apiKey = "311f1f45fee82242ab4086372ab360f5";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -85,12 +74,6 @@ function displayWeatherCondition(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
-}
-
-function searchCity(city) {
-  let apiKey = "311f1f45fee82242ab4086372ab360f5";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
 }
 
 function searchLocation(position) {
